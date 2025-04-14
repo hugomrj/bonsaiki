5b8dc0717bbc211b2790ea2f191347d2bcf7afce
Hugo Romero <hugomrj@gmail.com>
2025-04-13 21:09:40 -0400

    menucel

--- assets/menu.js
+++ assets/menu.js
@@ -1,47 +1,62 @@
 function genNavbar() {
     const navbarHTML = `
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="index.html">
+                    Bonsaiki
+                </a>
+                
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBonsaiki">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+                
 
-          <div class="container">
-              <div class="navbar-brand">
-                  <a class="navbar-item" href="index.html">
-                      Bonsaiki
-                  </a>
-      
-                  <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBonsaiki">
-                      <span aria-hidden="true"></span>
-                      <span aria-hidden="true"></span>
-                      <span aria-hidden="true"></span>
-                      <span aria-hidden="true"></span>
-                  </a>
-              </div>
-      
-              <div id="navbarBonsaiki" class="navbar-menu">
-                  <div class="navbar-start">
-                      <a class="navbar-item" href="index.html">
-                          Inicio
-                      </a>
-                      <a class="navbar-item" href="catalogo.html">
-                          Catálogo
-                      </a>
-                      <a class="navbar-item" href="blog.html">
-                          Blog
-                      </a>
-                      <a class="navbar-item" href="index.html#contacto">
-                          Contacto
-                      </a>
-                  </div>
-              </div>
-          </div>
-
+            </div>
+            <div id="navbarBonsaiki" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="index.html">
+                        Inicio
+                    </a>
+                    <a class="navbar-item" href="catalogo.html">
+                        Catálogo
+                    </a>
+                    <a class="navbar-item" href="blog.html">
+                        Blog
+                    </a>
+                    <a class="navbar-item" href="index.html#contacto">
+                        Contacto
+                    </a>
+                </div>
+            </div>
+        </div>
     `;
     return navbarHTML;
-  }
-  
+}
+
+
+
+
 
 export function initMenu() {
     const navMain = document.getElementById("nav_main");
     if (navMain) {
         navMain.innerHTML = genNavbar();
+
+        // Simplificando el script para depuración
+        const navbarBurger = document.querySelector('.navbar-burger');
+        const navbarMenu = document.getElementById('navbarBonsaiki');
+
+        if (navbarBurger && navbarMenu) {
+            navbarBurger.addEventListener('click', () => {
+                navbarBurger.classList.toggle('is-active');
+                navbarMenu.classList.toggle('is-active');
+            });
+        } else {
+            console.error("Navbar burger or menu not found.");
+        }
     } else {
         console.error("No se encontró el elemento con el id 'nav_main'.");
     }
